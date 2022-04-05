1a5257150eb3e750814651f342bffa78307c3989
Aranya Meas <ameas36@gmail.com>
2022-04-05 02:17:26 -0700

    Created main view and added baby header bar

---
+++ src/content/MainWindow.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+
+export const MainWindow = () => {
+    return (
+        <Box>
+            <AppBar className={"App-Header"}>SHOPPING LIST</AppBar>
+        </Box>
+    );
+}
