237071da2a58fa2c0b6417db2a328943b6c37a5d
Aranya Meas <ameas36@gmail.com>
2022-04-06 00:37:34 -0700

    Empty list page done

--- src/content/MainWindow.js
+++ src/content/MainWindow.js
@@ -1,12 +1,40 @@
-import React from "react";
-import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { AppBar, Box, Button } from "@mui/material";
+import { display, height } from "@mui/material/node_modules/@mui/system";
+// import { Loading } from "./Loading";
+// import { Add } from "./Add";
+// import { Edit } from "./Edit";
+// import { Delete } from "./Delete";
 
-export const MainWindow = () => {
+export const MainWindow = (props) => {
+    // const [shoppingList, setShoppingList] = useState([]);
+
+    // const addItem = () => {
+
+    // }
+
+    // const editItem = () => {
+
+    // }
+
+    // const deleteItem = () => {
+
+    // }
+
     return (
-        <Box>
+        <div>
             <AppBar className={"App-header"}>
                 <span className={"Header-title font-dosis"}>SHOPPING LIST</span>
             </AppBar>
-        </Box>
+            
+            <div className="Empty-page">
+                <div className="Empty-box">
+                    <div className="Empty-content">
+                        <span className="Empty-text font-nunito"> Your shopping list is empty :(</span>
+                        <Button variant="contained"><span className="font-nunito Button-text">Add your first item</span></Button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
-}
+};
