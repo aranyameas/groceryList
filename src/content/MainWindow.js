ef1152fc1f18c1dd08b8e2ed867f468f98f01cc6
Aranya Meas <ameas36@gmail.com>
2022-04-05 23:23:05 -0700

    Header done

--- src/content/MainWindow.js
+++ src/content/MainWindow.js
@@ -4,7 +4,9 @@ import { Box, AppBar, Toolbar, Typography } from "@mui/material";
 export const MainWindow = () => {
     return (
         <Box>
-            <AppBar className={"App-Header"}>SHOPPING LIST</AppBar>
+            <AppBar className={"App-header"}>
+                <span className={"Header-title font-dosis"}>SHOPPING LIST</span>
+            </AppBar>
         </Box>
     );
 }
