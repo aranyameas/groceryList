ba9319ac3b6698a9a483b39babff2ad97daf00ec
Aranya Meas <ameas36@gmail.com>
2022-04-07 00:14:22 -0700

    Start of populated list page. Moved empty list page to its own file

--- src/content/MainWindow.js
+++ src/content/MainWindow.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { AppBar, Backdrop, Box, Button, Slide, Modal, Typography } from "@mui/material";
-import { display, height } from "@mui/material/node_modules/@mui/system";
+import { AppBar, Backdrop, Box, Button, List, ListItem, ListItemIcon, ListItemText, Checkbox, Slide, Modal, Typography } from "@mui/material";
+import { EmptyList } from "./EmptyList";
 // import { Loading } from "./Loading";
 // import { Add } from "./Add";
 // import { Edit } from "./Edit";
@@ -18,10 +18,12 @@ const style = {
   };
 
 export const MainWindow = (props) => {
-    // const [shoppingList, setShoppingList] = useState([]);
-    const [addOpen, setAddOpen] = useState(false);
-    const handleAddOpen = () => setAddOpen(true);
-    const handleAddClose = () => setAddOpen(false);
+    const [shoppingList, setShoppingList] = useState([
+    {
+        "Item Name": "Tomatoes",
+        "Description": "Walmart",
+        "Quantity": 5
+    }]);
 
     // const addItem = () => {
 
@@ -40,34 +42,14 @@ export const MainWindow = (props) => {
             <AppBar className={"App-header"}>
                 <span className={"Header-title font-dosis"}>SHOPPING LIST</span>
             </AppBar>
+
+            {/* <EmptyList /> */}
             
-            <div className="Empty-page">
-                <div className="Empty-box">
-                    <div className="Empty-content">
-                        <span className="Empty-text font-nunito"> Your shopping list is empty :(</span>
-                        <Button variant="contained" onClick={handleAddOpen}><span className="font-nunito Button-text">Add your first item</span></Button>
-                        <Modal
-                            aria-labelledby="transition-modal-title"
-                            aria-describedby="transition-modal-description"
-                            open={addOpen}
-                            onClose={handleAddClose}
-                            closeAfterTransition
-                            BackdropComponent={Backdrop}
-                            BackdropProps={{
-                            timeout: 5,
-                            }}
-                        >
-                            <Slide direction="left" in={addOpen}>
-                            <Box sx={style}>
-                                <Typography id="transition-modal-title" variant="h6" component="h2">
-                                Text in a modal
-                                </Typography>
-                                <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                                </Typography>
-                            </Box>
-                            </Slide>
-                        </Modal>
+            <div className="List-page">
+                <div className="List-content">
+                    <div className="List-header">
+                        <span className="font-nunito Title">Your Items</span>
+                        <Button variant="contained"><span className="font-nunito Button-text">Add Item</span></Button>
                     </div>
                 </div>
             </div>
